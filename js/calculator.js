c6524cdf4c3cf53e97a6e0654e6b0cc34cb895d7
Jackie Yu <jackieyu247@gmail.com>
2022-08-10 20:44:06 +0100

    remove global variables and restructure code

--- js/calculator.js
+++ js/calculator.js
@@ -1,12 +1,44 @@
-displayInputs = [];
-previousInput = 0;
-result = [];
-
-function start(){
-    createNumberPad();
-    operatorBtns();
+function calculator(){
+    let currInput = [];
+    let previousInput = [];
+    let result = 0;
+    let operator = "";
+    createNumberPad(currInput);
+    // Select operator buttons
+    const addBtn = document.querySelector('.add');
+    const subBtn = document.querySelector('.sub');
+    const multBtn = document.querySelector('.mult');
+    const divBtn = document.querySelector('.divide');
+    const clearBtn = document.querySelector('.clear');
+    const equalsBtn = document.querySelector(".equals");
+    // Assign functions
+    addBtn.onclick = () => {
+        // Assign current operator to addition
+        operator = '+';
+        // if a previous input exists. we want to simply display the result of adding both numbers.
+        if(previousInput.length === 1)
+        {
+            result = operate(operator,currInput,previousInput);
+        }
+        // When add button clicked, store the current input into previous input array.
+        storeLastInput(previousInput,currInput.join(""));
+        updateEquationDisplay(previousInput);
+        clearCurrentInput(currInput);
+        console.log("previous input: " + previousInput);
+    }
+    //subBtn.onclick = operate(subtract,a,b);
+    //multBtn.onclick = operate(multiply,a,b);
+    //divBtn.onclick = operate(divide,a,b);
+    clearBtn.onclick = () => {
+        clearDisplay();
+    }
+    equalsBtn.onclick = () => {
+        result = operate(operator,previousInput,currInput);
+        clearEquationDisplay()
+        showResult(result);
+    }
 }
-function createNumberPad(){
+function createNumberPad(currInput){
     // Select button div container
     const buttonDiv = document.querySelector(".numbers");
     // Create 10 numbers 0-9
@@ -17,77 +49,74 @@ function createNumberPad(){
         button.type = "button";
         // button functions
         button.onclick = () => {
-            displayInputs.push(parseInt(button.textContent,10));
-            updateDisplay();
-            console.log(displayInputs);
+            currInput.push(button.textContent);
+            updateInputDisplay(currInput);
+            console.log(typeof currInput);
+            console.log("Current input: " + currInput);
         };
         // append button to div container
         buttonDiv.appendChild(button);
     }
 }
-function operatorBtns(){
-    // Select operator buttons
-    let addBtn = document.querySelector('.add');
-    let subBtn = document.querySelector('.sub');
-    let multBtn = document.querySelector('.mult');
-    let divBtn = document.querySelector('.divide');
-    let clearBtn = document.querySelector('.clear');
-    // Assign functions
-    addBtn.onclick = () => {
-        operate("add",previousInput);
-        console.log("previous input: " + previousInput);
-    }
-    //subBtn.onclick = operate(subtract,a,b);
-    //multBtn.onclick = operate(multiply,a,b);
-    //divBtn.onclick = operate(divide,a,b);
-    clearBtn.onclick = () => {
-        clearDisplay();
-        clearHistory();
-    }
-}
-
-function updateDisplay(){
-    const history = document.querySelector(".history");
-    const display = document.querySelector(".display");
-    // If previous input exists. Display on screen
-    if(previousInput > 0){
-        history.textContent = previousInput;
-    }
-    // If the first input is not 0, update the display content.
-    if(displayInputs[0] != "0") {
-        display.textContent = displayInputs.join("");
-        return true;
-    }
-
-    // If first input is 0, remove from array.
-    displayInputs.shift();
-}
-
-
-function clearDisplay(){
-    const display = document.querySelector(".display");
-    display.textContent = "0";
-    if(displayInputs.length > 0){
-        displayInputs.splice(0);
+// Arithmetic helper functions
+function add(a,b){return a+b;}
+function subtract(a,b){return a -b;}
+function multiply(a,b){return a * b;}
+function divide(a,b){return a / b;}
+// General functions
+function storeLastInput(prevInput,input){
+    if(input.length > 0){
+        prevInput.push(input);
         return true;
     }
     return false;
-    
 }
+function updateInputDisplay(currInput){
+    // Select display container
+    const inputDisplay = document.querySelector(".display");
+    // Update input display if user presses non zero key.
+    if(currInput[0] != "0") {
+        inputDisplay.textContent = currInput.join("");
+        return true;
+    }
+    // If first input is 0, remove from array.
+    calc.currInput.shift();
+}
+function showResult(result){
+        // Select display container
+        const inputDisplay = document.querySelector(".display");
+        inputDisplay.textContent = result;
 
-function clearHistory(){
-    const history = document.querySelector(".history");
-    history.textContent = "";
-    previousInput = 0;
+}
+function updateEquationDisplay(prevInput){
+    const eqDisplay = document.querySelector(".equation");
+    eqDisplay.textContent = prevInput.join(" + ");
     return true;
 }
-
-function operate(operator,previousInput){
-    if(operator == "add"){
-        storeInput();
-        clearDisplay()
-        updateDisplay();
+function clearCurrentInput(currInput){
+    const inputDisplay = document.querySelector(".display");
+    if(currInput.length > 0){
+        currInput.splice(0);
+        inputDisplay.textContent = "0";
+        return true;
     }
+    return false;
+}
+function clearEquationDisplay(){
+    const eqDisplay = document.querySelector(".equation");
+    eqDisplay.textContent = "";
+    return true;
+}
+function operate(operator,currentInput,previousInput){
+    if(operator == "+"){
+        let result = add(
+            parseInt(currentInput.join(""),10),
+            parseInt(previousInput.join(""),10)
+        );
+        console.log("Final result: " + result);
+        return result;
+    }
+    /*
     else if(operator == subtract){
         return subtract(a,b);
     }
@@ -99,13 +128,16 @@ function operate(operator,previousInput){
     }
     else{
         return false;
-    }
+    }*/
 }
-// Arithmetic helper functions
-function add(a,b){return a+b;}
-function subtract(a,b){return a -b;}
-function multiply(a,b){return a * b;}
-function divide(a,b){return a / b;}
-// General functions
-function storeInput(){return previousInput += parseInt(displayInputs.join(""),10);}
-start();
+function clearHistory(){
+    const history = document.querySelector(".history");
+    history.textContent = "";
+    previousInput = 0;
+    if(displayInputs.length > 0){
+        displayInputs.splice(0);
+        return true;
+    }
+    return true;
+}
+calculator();
